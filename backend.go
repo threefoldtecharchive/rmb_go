89aa68dfecfccab7b6da75f3c6143f7be0cccd9d
OmarElawady <omarelawady1998@gmail.com>
2021-08-26 16:53:10 +0200

    implement redis backend to the new interface

--- backend.go
+++ backend.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -33,12 +34,22 @@ type Envelope struct {
 	Tag Tag
 }
 type Backend interface {
+	Next(ctx context.Context, timeout time.Duration) (Envelope, error)
 	QueueReply(ctx context.Context, msg Message) error // method name
 	QueueRemote(ctx context.Context, msg Message) error
-	Next(ctx context.Context, timeout time.Duration) (Envelope, error)
+
+	IncrementID(ctx context.Context, id int) (int64, error)
+
 	PushToBacklog(ctx context.Context, msg Message, id string) error
+	PopMessageFromBacklog(ctx context.Context, id string) (Message, error)
+
+	QueueCommand(ctx context.Context, msg Message) error
+	PushProcessedMessage(ctx context.Context, msg Message) error
+
 	QueueRetry(ctx context.Context, msg Message) error
-	PopMessageFromBacklog(ctx context.Context, id string) error
+	PopRetryMessages(ctx context.Context, olderThan time.Duration) ([]Message, error)
+
+	PopExpiredBacklogMessages(ctx context.Context) ([]Message, error)
 }
 
 type BackendInterface interface {
@@ -76,6 +87,158 @@ func (r *RedisBackend) Next(ctx context.Context, timeout time.Duration) (Envelop
 	return envelope, nil
 }
 
+func (r *RedisBackend) QueueReply(ctx context.Context, msg Message) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.LPush(ctx, "msgbus.system.reply", bytes).Result()
+	if err != nil {
+		return errors.Wrap(err, "failed to push reply message to redis")
+	}
+	return nil
+}
+
+func (r *RedisBackend) QueueRemote(ctx context.Context, msg Message) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.LPush(ctx, "msgbus.system.remote", bytes).Result()
+	if err != nil {
+		return errors.Wrap(err, "failed to push remote message to redis")
+	}
+	return nil
+}
+
+func (r *RedisBackend) IncrementID(ctx context.Context, id int) (int64, error) {
+	cnt, err := r.client.Incr(ctx, fmt.Sprintf("msgbus.counter.%d", id)).Result()
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to increment redis counter")
+	}
+	return cnt, nil
+}
+
+func (r *RedisBackend) PushToBacklog(ctx context.Context, msg Message, id string) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.HSet(ctx, "msgbus.system.backlog", id, bytes).Result()
+	if err != nil {
+		return errors.Wrap(err, "failed to add to redis msgbus.system.backlog")
+	}
+	return nil
+}
+
+func (r *RedisBackend) PopMessageFromBacklog(ctx context.Context, id string) (Message, error) {
+	msg := Message{}
+
+	bytes, err := r.client.HGet(ctx, "msgbus.system.backlog", id).Result()
+
+	if err == redis.Nil {
+		return msg, ErrNotAvailable
+	} else if err != nil {
+		// looks like most errors here doesn't need wrapping, looks like a string from its parent
+		return msg, errors.Wrap(err, "error fetching message from backlog")
+	}
+
+	if err := json.Unmarshal([]byte(bytes), &msg); err != nil {
+		return msg, errors.Wrap(err, "couldn't parse json")
+	}
+	return msg, nil
+}
+
+func (r *RedisBackend) QueueCommand(ctx context.Context, msg Message) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.LPush(ctx, fmt.Sprintf("msgbus.%s", msg.Command), bytes).Result()
+	return err
+}
+
+func (r *RedisBackend) PushProcessedMessage(ctx context.Context, msg Message) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.LPush(ctx, msg.Retqueue, bytes).Result()
+	return err
+}
+
+func (r *RedisBackend) QueueRetry(ctx context.Context, msg Message) error {
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode into json")
+	}
+
+	_, err = r.client.HSet(ctx, "msgbus.system.retry", msg.ID, bytes).Result()
+	return err
+}
+
+func (r *RedisBackend) PopRetryMessages(ctx context.Context, olderThan time.Duration) ([]Message, error) {
+	lines, err := r.client.HGetAll(ctx, "msgbus.system.retry").Result()
+
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't read retry messages")
+	}
+
+	msgs := []Message{}
+
+	now := time.Now().Unix()
+	for key, value := range lines {
+		var msg Message
+		if err := json.Unmarshal([]byte(value), &msg); err != nil {
+			// should it be popped off the retry queue?
+			log.Error().Err(errors.Wrap(err, "couldn't parse json")).Msg("handling retry queue")
+			continue
+		}
+		if now > msg.Epoch+5 {
+			if _, err := r.client.HDel(ctx, "msgbus.system.retry", key).Result(); err != nil {
+				log.Error().Err(err).Msg("error deleting retry message")
+			} else {
+				msgs = append(msgs, msg)
+			}
+		}
+	}
+	return msgs, err
+}
+
+func (r *RedisBackend) PopExpiredBacklogMessages(ctx context.Context) ([]Message, error) {
+	lines, err := r.client.HGetAll(ctx, "msgbus.system.backlog").Result()
+
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't read backlog messages")
+	}
+
+	msgs := []Message{}
+
+	now := time.Now().Unix()
+	for key, value := range lines {
+		var msg Message
+		if err := json.Unmarshal([]byte(value), &msg); err != nil {
+			// should it be popped off the backlog queue?
+			log.Error().Err(errors.Wrap(err, "couldn't parse json")).Msg("handling backlog queue")
+			continue
+		}
+
+		if msg.Epoch+msg.Expiration < now {
+			if _, err := r.client.HDel(ctx, "msgbus.system.backlog", key).Result(); err != nil {
+				log.Error().Err(err).Msg("error deleting backlog expired message")
+			} else {
+				msg.ID = key
+				msgs = append(msgs, msg)
+			}
+		}
+	}
+	return msgs, err
+}
 func (r RedisBackend) HGet(ctx context.Context, key string, field string) (string, error) {
 	return r.client.HGet(ctx, key, field).Result()
 }
--- server.go
+++ server.go
@@ -68,7 +68,6 @@ type MBusCtx struct {
 }
 
 type App struct {
-	redis    BackendInterface
 	backend  Backend
 	twin     int
 	resolver TwinResolver
@@ -86,15 +85,17 @@ func successReply(w http.ResponseWriter) {
 }
 
 func (a *App) respondWithError(ctx context.Context, msg Message, err error) error {
-	return nil
+	msg.Err = err.Error()
+	return a.backend.PushProcessedMessage(ctx, msg)
 }
 
 func (a *App) msgNeedsRetry(ctx context.Context, msg Message, err error) error {
-	if msg.Retry == 0 {
+	if msg.Retry <= 0 {
 		if err := a.respondWithError(ctx, msg, errors.Wrap(err, "all retries done")); err != nil {
 			return errors.Wrap(err, "failed to respond to the caller with the proper err")
 		}
 	} else {
+		msg.Retry--
 		if err := a.backend.QueueRetry(ctx, msg); err != nil {
 			return errors.Wrap(err, "failed to queue msg for retry")
 		}
@@ -110,7 +111,7 @@ func (a *App) handleFromLocalItem(ctx context.Context, msg Message, dst int) err
 	update.TwinSrc = a.twin
 	update.TwinDst = []int{dst}
 
-	err := error(nil)
+	var err error = nil
 	defer func() {
 		if err != nil {
 			if repErr := a.msgNeedsRetry(ctx, msg, err); repErr != nil {
@@ -158,44 +159,26 @@ func (a *App) handleFromLocal(ctx context.Context, msg Message) error {
 }
 
 func (a *App) handleFromRemote(ctx context.Context, msg Message) error {
-
 	log.Info().Str("queue", fmt.Sprintf("msgbus.%s", msg.Command)).Msg("forwarding to local service")
 
 	// forward to local service
-	a.redis.LPush(ctx, fmt.Sprintf("msgbus.%s", msg.Command), nil)
-	return nil
+	return a.backend.QueueCommand(ctx, msg)
 }
 
 func (a *App) handleFromReplyForMe(ctx context.Context, msg Message) error {
 	log.Info().Msg("message reply for me, fetching backlog")
 
-	retval, err := a.redis.HGet(ctx, "msgbus.system.backlog", msg.ID)
-
-	if err == redis.Nil {
-		return errors.New(fmt.Sprintf("couldn't find key %s", msg.ID))
-	} else if err != nil {
-		return err
-	}
-	original := Message{}
-
-	if err := json.Unmarshal([]byte(retval), &original); err != nil {
-		return errors.Wrap(err, "couldn't parse json")
-	}
-	update := msg
-
-	// restore return queue name for the caller
-	update.Retqueue = original.Retqueue
-
-	value, err := json.Marshal(update)
-
+	original, err := a.backend.PopMessageFromBacklog(ctx, msg.ID)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error fetching message from backend")
 	}
-	// forward reply to original sender
-	a.redis.LPush(ctx, update.Retqueue, value)
+	// restore return queue name for the caller
+	msg.Retqueue = original.Retqueue
 
-	// remove from backlog
-	a.redis.HDel(ctx, "msgbus.system.backlog", msg.ID)
+	err = a.backend.PushProcessedMessage(ctx, msg)
+	if err != nil {
+		return errors.Wrap(err, "error pushing the reply message")
+	}
 	return nil
 }
 
@@ -213,41 +196,9 @@ func (a *App) handleFromReplyForward(ctx context.Context, msg Message) error {
 	err = r.SendReply(msg)
 
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error forwarding reply from local service to the caller rmb")
 	}
 
-	return nil
-}
-
-func (a *App) requestNeedsRetry(ctx context.Context, msg Message, update Message) error {
-	log.Info().Msg("could not send message to remote msgbus")
-
-	// restore 'update' to original state
-	update.Retqueue = msg.Retqueue
-
-	if update.Retry == 0 {
-		log.Info().Msg("no more retry, replying with error")
-		update.Err = "could not send request and all retries done"
-		output, err := json.Marshal(update)
-		if err != nil {
-			return errors.Wrap(err, "couldn't parse json")
-		}
-		a.redis.LPush(ctx, update.Retqueue, output)
-		return nil
-	}
-	log.Info().Int("retry", update.Retry).Msg("updating retry and adding to retry list")
-
-	// remove one retry
-	update.Retry--
-	update.Epoch = time.Now().Unix()
-
-	value, err := json.Marshal(update)
-
-	if err != nil {
-		return errors.Wrap(err, "couldn't parse json")
-	}
-
-	a.redis.HSet(ctx, "msgbus.system.retry", update.ID, value)
 	return nil
 }
 
@@ -263,52 +214,21 @@ func (a *App) handleFromReply(ctx context.Context, msg Message) error {
 	return nil
 }
 
-func (a *App) handleInternalHgetall(lines map[string]string) []HSetEntry {
-	entries := []HSetEntry{}
-
-	// build usable list from redis response
-	for key, value := range lines {
-		message := Message{}
-
-		if err := json.Unmarshal([]byte(value), &message); err != nil {
-			log.Error().Err(errors.Wrap(err, "couldn't parse json")).Msg("handling retry queue")
-			continue
-		}
-
-		entries = append(entries, HSetEntry{
-			Key:   key,
-			Value: message,
-		})
-	}
-
-	return entries
-}
-
 func (a *App) handleRetry(ctx context.Context) error {
 	log.Debug().Msg("checking retries")
 
-	lines, err := a.redis.HGetAll(ctx, "msgbus.system.retry")
-
+	entries, err := a.backend.PopRetryMessages(ctx, 5*time.Second)
 	if err != nil {
 		return errors.Wrap(err, "couldn't read retry messages")
 	}
 
-	entries := a.handleInternalHgetall(lines)
-
-	now := time.Now().Unix()
-
 	// iterate over each entries
 	for _, entry := range entries {
-		if now > entry.Value.Epoch+5 { // 5 sec debug
-			log.Info().Str("key", entry.Key).Msg("retry needed")
+		log.Info().Str("key", entry.ID).Msg("retry needed")
 
-			// remove from retry list
-			a.redis.HDel(ctx, "msgbus.system.retry", entry.Key)
-
-			// re-call sending function, which will succeed
-			// or put it back to retry
-			a.handleFromLocalItem(ctx, entry.Value, entry.Value.TwinDst[0])
-		}
+		// re-call sending function, which will succeed
+		// or put it back to retry
+		a.handleFromLocalItem(ctx, entry, entry.TwinDst[0])
 	}
 	return nil
 }
@@ -316,35 +236,16 @@ func (a *App) handleRetry(ctx context.Context) error {
 func (a *App) handleScrubbing(ctx context.Context) error {
 	log.Debug().Msg("scrubbing")
 
-	lines, err := a.redis.HGetAll(ctx, "msgbus.system.backlog")
+	entries, err := a.backend.PopExpiredBacklogMessages(ctx)
 
 	if err != nil {
 		return errors.Wrap(err, "couldn't read backlog messages")
 	}
 
-	entries := a.handleInternalHgetall(lines)
-
-	now := time.Now().Unix()
-
 	// iterate over each entries
 	for _, entry := range entries {
-		if entry.Value.Expiration == 0 {
-			// avoid infinite expiration, fallback to 1h
-			entry.Value.Expiration = 3600
-		}
-
-		if entry.Value.Epoch+entry.Value.Expiration < now {
-			log.Debug().Str("key", entry.Key).Msg("expired")
-
-			entry.Value.Err = fmt.Sprintf("request timeout (expiration reached, %d)", entry.Value.Expiration)
-			output, err := json.Marshal(entry.Value)
-			if err != nil {
-				log.Error().Err(err).Msg("couldn't parse json")
-				continue
-			}
-			a.redis.LPush(ctx, entry.Value.Retqueue, output)
-			a.redis.HDel(ctx, "msgbus.system.backlog", entry.Key)
-		}
+		log.Debug().Str("key", entry.ID).Msg("expired")
+		a.respondWithError(ctx, entry, fmt.Errorf("request timeout (expiration reached, %d)", entry.Expiration))
 	}
 	return nil
 }
@@ -383,6 +284,7 @@ func (a *App) runServer(ctx context.Context) {
 
 		if err := envelope.Valid(); err != nil {
 			log.Error().Err(err).Msg("received invalid message")
+			a.respondWithError(ctx, envelope.Message, errors.Wrap(err, "received invalid message"))
 			//TODO: try to send the reply with the validation error
 			// back to the caller, even if u can't process the mssage.
 		}
@@ -460,13 +362,13 @@ func NewServer(substrate string, redisServer string, twin int) (*App, error) {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	backend := RedisBackend{client: rdb}
+	backend := &RedisBackend{client: rdb}
 	resolver, err := NewTwinResolver(substrate)
 	if err != nil {
 		return nil, errors.Wrap(err, "couldn't get a client to explorer resolver")
 	}
 	a := &App{
-		redis:    backend,
+		backend:  backend,
 		twin:     twin,
 		resolver: resolver,
 		server: &http.Server{
