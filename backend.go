2c956584d17a5fd5751b666253aad99f91063a59
OmarElawady <omarelawady1998@gmail.com>
2021-08-26 17:21:25 +0200

    remove old interface

--- backend.go
+++ backend.go
@@ -246,30 +246,3 @@ func (r *RedisBackend) PopExpiredBacklogMessages(ctx context.Context) ([]Message
 	}
 	return msgs, err
 }
-func (r RedisBackend) HGet(ctx context.Context, key string, field string) (string, error) {
-	return r.client.HGet(ctx, key, field).Result()
-}
-
-func (r RedisBackend) HGetAll(ctx context.Context, key string) (map[string]string, error) {
-	return r.client.HGetAll(ctx, key).Result()
-}
-
-func (r RedisBackend) HDel(ctx context.Context, key string, field string) (int64, error) {
-	return r.client.HDel(ctx, key, field).Result()
-}
-
-func (r RedisBackend) LPush(ctx context.Context, key string, value []byte) (int64, error) {
-	return r.client.LPush(ctx, key, value).Result()
-}
-
-func (r RedisBackend) BLPop(ctx context.Context, timeout time.Duration, keys ...string) ([]string, error) {
-	return r.client.BLPop(ctx, timeout, keys...).Result()
-}
-
-func (r RedisBackend) Incr(ctx context.Context, key string) (int64, error) {
-	return r.client.Incr(ctx, key).Result()
-}
-
-func (r RedisBackend) HSet(ctx context.Context, key string, field string, value []byte) (int64, error) {
-	return r.client.HSet(ctx, key, field, value).Result()
-}
